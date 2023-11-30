0f6d16cf8713b96ef00c225442727da5ac5ae78a
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2023-11-30 18:51:30 -0300

    removes unused content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "kuni";
+export const SITE_DESCRIPTION =
+  "I talk about #programming, #frontend and #javascript";
