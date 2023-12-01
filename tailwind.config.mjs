145d371412669889e0027a72820a389a66951737
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2023-11-30 22:26:52 -0300

    configures starter theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#aaf683',
+				secondary: '#7768ae',
+				content: '#f8f7ff',
+				'content-accent': '#fff',
+				background: '#051014',
+				'background-accent': '#6a706e',
+				'background-content': '#2e2f2f',
+			},
+		},
 	},
 	plugins: [],
 };
