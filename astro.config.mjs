b72e48b38b9ba305d5df58129a227115e9d69373
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2023-12-07 21:34:44 -0300

    fix metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kuni-dev.vercel.app/',
 	integrations: [mdx(), sitemap(), tailwind(), react()],
 });
